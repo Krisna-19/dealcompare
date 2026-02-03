76dd8800c06fb432b1bd70ef59e701d469ebefe5
Krishna Theertha <krisnatg19@gmail.com>
2026-02-03 10:56:48 +0530

    Updated

--- dealcompare-ui/src/main.jsx
+++ dealcompare-ui/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms";
+import Affiliate from "./pages/Affiliate";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+      <Route path="privacy" element={<Privacy />} />
+      <Route path="terms" element={<Terms />} />
+      <Route path="affiliate-disclosure" element={<Affiliate />} />
+    </Route>
+    </Routes>
+  </BrowserRouter>
+);
