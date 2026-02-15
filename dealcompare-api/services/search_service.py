898557a1bd3d6ccee2e061fb85a8fe254b39b175
Krishna Theertha <krisnatg19@gmail.com>
2026-02-15 17:25:30 +0530

    UPdated

--- dealcompare-api/services/search_service.py
+++ dealcompare-api/services/search_service.py
@@ -1,11 +1,25 @@
-from platforms import amazon, myntra, ajio
+from platforms.amazon import search_amazon
+from platforms.myntra import search_myntra
+from platforms.ajio import search_ajio
+
 
 def search_all(query: str):
 
     products = []
 
-    products.extend(amazon.search(query))
-    products.extend(myntra.search(query))
-    products.extend(ajio.search(query))
+    try:
+        products.extend(search_amazon(query))
+    except Exception as e:
+        print("Amazon error:", e)
+
+    try:
+        products.extend(search_myntra(query))
+    except Exception as e:
+        print("Myntra error:", e)
+
+    try:
+        products.extend(search_ajio(query))
+    except Exception as e:
+        print("Ajio error:", e)
 
     return products
