a73d1848564057db86d5d409b62fdc2e5f458a50
Krishna Theertha <krisnatg19@gmail.com>
2026-02-15 13:32:54 +0530

    updated

--- dealcompare-api/platforms/amazon.py
+++ dealcompare-api/platforms/amazon.py
@@ -2,22 +2,15 @@ from models.product_model import Product
 from utils.category import detect_category
 from urllib.parse import quote_plus
 
-def search(query: str):
-
-    category = detect_category(query)
-    encoded = quote_plus(query)
-
-    url = f"https://www.amazon.in/s?k={encoded}&tag=dealcompare19-21"
-
-    product = Product(
-        title=query.title(),
-        platform="Amazon",
-        price_value=0,
-        price_display="Check price",
-        rating=None,
-        url=url,
-        image="",
-        category=category
-    )
-
-    return [product]
+def search_amazon(query: str):
+    return [
+        {
+            "title": query.title(),
+            "price_value": 0.0,
+            "price_display": "Check price",
+            "platform": "Amazon",
+            "url": f"https://www.amazon.in/s?k={query.replace(' ', '+')}&tag=dealcompare19-21",
+            "rating": None,
+            "image": ""
+        }
+    ]
