1dd70a04b13e4092eaeb4afa4971c39c58c0b693
Krishna Theertha <krisnatg19@gmail.com>
2026-02-25 11:21:17 +0530

    updated

--- dealcompare-api/platforms/ajio.py
+++ dealcompare-api/platforms/ajio.py
@@ -1,22 +1,19 @@
-from models.product_model import Product
-from utils.category import detect_category
 from urllib.parse import quote_plus
+
 
 def search_ajio(query: str):
 
-    category = detect_category(query)
     encoded = quote_plus(query)
-
     url = f"https://www.ajio.com/search/?text={encoded}"
 
-    product = Product(
-        title=query.title(),
-        platform="Ajio",
-        price_value=0,
-        price_display="Check price",
-        rating=None,
-        url=url,
-        image=""
-    )
+    product = {
+        "title": query.title(),
+        "platform": "Ajio",
+        "price_value": 0,
+        "price_display": "Check price",
+        "rating": None,
+        "url": url,
+        "image": ""
+    }
 
     return [product]
--- dealcompare-api/platforms/myntra.py
+++ dealcompare-api/platforms/myntra.py
@@ -1,22 +1,19 @@
-from models.product_model import Product
-from utils.category import detect_category
 from urllib.parse import quote_plus
+
 
 def search_myntra(query: str):
 
-    category = detect_category(query)
     encoded = quote_plus(query)
-
     url = f"https://www.myntra.com/{encoded}"
 
-    product = Product(
-        title=query.title(),
-        platform="Myntra",
-        price_value=0,
-        price_display="Check price",
-        rating=None,
-        url=url,
-        image=""
-    )
+    product = {
+        "title": query.title(),
+        "platform": "Myntra",
+        "price_value": 0,
+        "price_display": "Check price",
+        "rating": None,
+        "url": url,
+        "image": ""
+    }
 
     return [product]
