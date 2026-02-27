8b62aa37d88e71cff13d64fed4f3dbd67fbf3145
Krishna Theertha <krisnatg19@gmail.com>
2026-02-27 10:58:39 +0530

    Updated

--- dealcompare-api/services/ranking_service.py
+++ dealcompare-api/services/ranking_service.py
@@ -1,5 +1,6 @@
 import re
 from rapidfuzz import fuzz
+from utils.text_util import normalize_text, extract_model_number, extract_storage
 
 
 # -----------------------------
@@ -64,17 +65,36 @@ def strict_model_match(query: str, title: str):
 # -----------------------------
 def calculate_match_score(query: str, title: str):
 
-    query_norm = normalize(query)
-    title_norm = normalize(title)
+    query_norm = normalize_text(query)
+    title_norm = normalize_text(title)
 
-    base_score = fuzz.token_set_ratio(query_norm, title_norm)
+    query_model = extract_model_number(query_norm)
+    title_model = extract_model_number(title_norm)
 
-    # bonus for brand presence
-    brand_bonus = 0
-    if "iphone" in query_norm and "iphone" in title_norm:
-        brand_bonus += 10
+    query_storage = extract_storage(query_norm)
+    title_storage = extract_storage(title_norm)
 
-    if "apple" in query_norm and "apple" in title_norm:
-        brand_bonus += 5
+    score = 0
 
-    return base_score + brand_bonus
+    # Model match
+    if query_model and title_model and query_model == title_model:
+        score += 40
+    else:
+        return -50
+
+    # Storage match
+    if query_storage:
+        if query_storage == title_storage:
+            score += 40
+        else:
+            return -40
+
+    # Keyword overlap
+    query_words = set(query_norm.split())
+    title_words = set(title_norm.split())
+
+    overlap = query_words.intersection(title_words)
+
+    score += len(overlap) * 5
+
+    return score
