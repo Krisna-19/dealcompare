0a0710ac8f0573266169fd7efcccda18c7847626
Krishna Theertha <krisnatg19@gmail.com>
2026-02-27 11:04:20 +0530

    Updated

--- dealcompare-api/services/ranking_service.py
+++ dealcompare-api/services/ranking_service.py
@@ -1,6 +1,6 @@
 import re
 from rapidfuzz import fuzz
-from utils.text_util import normalize_text, extract_model_number, extract_storage
+from utils.text_utils import normalize_text, extract_model_number, extract_storage
 
 
 # -----------------------------
