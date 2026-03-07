4955a1c811b7075354118ac46c0695ff205811cc
Krishna Theertha <krisnatg19@gmail.com>
2026-03-07 13:12:25 +0530

    Updated

--- dealcompare-api/app/services/search_service.py
+++ dealcompare-api/app/services/search_service.py
@@ -7,6 +7,8 @@ from app.scrapers.ajio import search_ajio
 
 async def search_all(query: str):
 
+    print(f"\n🔎 Searching for: {query}")
+
     amazon_task = asyncio.to_thread(search_amazon, query)
     myntra_task = asyncio.to_thread(search_myntra, query)
     ajio_task = asyncio.to_thread(search_ajio, query)
@@ -17,8 +19,18 @@ async def search_all(query: str):
         ajio_task
     )
 
+    amazon_products = results[0]
+    myntra_products = results[1]
+    ajio_products = results[2]
+
+    print("Amazon results:", len(amazon_products))
+    print("Myntra results:", len(myntra_products))
+    print("Ajio results:", len(ajio_products))
+
     all_products = []
     for r in results:
         all_products.extend(r)
+
+    print("Total products collected:", len(all_products))
 
     return all_products
